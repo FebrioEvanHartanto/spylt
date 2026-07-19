d985346e0f3783bdbd179e6a46a9b69a137bf28c
Rio <febrioevanhartanto@gmail.com>
2026-07-19 12:45:08 +0800

    Add message section with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import HeroSection from "./sections/HeroSection";
+import MessageSection from "./sections/MessageSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <MessageSection />
       <div className="h-dvh border border-red-500"></div>
     </div>
   );
