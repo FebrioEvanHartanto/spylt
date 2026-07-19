3faa1910dd3ad0502b933f04efcea06bba1185f1
Rio <febrioevanhartanto@gmail.com>
2026-07-20 00:13:14 +0800

    Add gsap SmootherScroll, add animation to flavor heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Navbar from "./components/navbar";
+import FlavorSection from "./sections/FlavorSection";
 import HeroSection from "./sections/HeroSection";
 import MessageSection from "./sections/MessageSection";
-import FlavorSection from "./sections/FlavorSection";
+import { ScrollSmoother } from "@/lib/gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Home() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
   return (
-    <div>
+    <main>
       <Navbar />
-      <HeroSection />
-      <MessageSection />
-      <FlavorSection />
-      <div className="h-dvh border-red-500 border-solid border-2"></div>
-    </div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-dvh border-red-500 border-solid border-2"></div>
+        </div>
+      </div>
+    </main>
   );
 }
--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,7 +1,8 @@
 import gsap from "gsap";
+import { ScrollSmoother } from "gsap/all";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 
-export { gsap, SplitText };
+export { gsap, SplitText, ScrollSmoother };
